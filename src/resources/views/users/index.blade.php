f021c696a1cf9f84ba2348b16bcf4aed5ab30ddd
Ivan <iv4art@ya.ru>
2016-01-22 00:36:58 +0300

    Удалён лишний div в разметке списке пользователей

--- src/resources/views/users/index.blade.php
+++ src/resources/views/users/index.blade.php
@@ -5,18 +5,16 @@
 
 <h3>Пользователи</h3>
 
-<div>
-	<ul class="nav nav-tabs">
-		<li class="active"><a href="#active" data-toggle="tab">Активные</a></li>
-		<li><a href="#deleted" data-toggle="tab">Удалённые</a></li>
-	</ul>
-	<div class="tab-content">
-		<div class="tab-pane active" id="active">
-			@include('Base::users._utils.users-list', ['users' => $active_users])
-		</div>
-		<div class="tab-pane" id="deleted">
-			@include('Base::users._utils.users-list', ['users' => $deleted_users])
-		</div>
+<ul class="nav nav-tabs">
+	<li class="active"><a href="#active" data-toggle="tab">Активные</a></li>
+	<li><a href="#deleted" data-toggle="tab">Удалённые</a></li>
+</ul>
+<div class="tab-content">
+	<div class="tab-pane active" id="active">
+		@include('Base::users._utils.users-list', ['users' => $active_users])
+	</div>
+	<div class="tab-pane" id="deleted">
+		@include('Base::users._utils.users-list', ['users' => $deleted_users])
 	</div>
 </div>
 
