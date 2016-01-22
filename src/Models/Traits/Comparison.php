16e479bd3dfc4b77cdeb143da09ad4c5dd7fd8fc
Ivan <iv4art@ya.ru>
2016-01-22 21:54:36 +0300

    Добавлен трейт для сравнения моделей

---
+++ src/Models/Traits/Comparison.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Chunker\Base\Models\Traits;
+
+use Illuminate\Database\Eloquent\Model;
+
+trait Comparison
+{
+	public function is(Model $model)
+	{
+		$is_class_equivalent = get_class($this) == get_class($model);
+		$is_key_equivalent = $this->getKey() == $model->getKey();
+
+		return $is_class_equivalent && $is_key_equivalent;
+	}
+}
