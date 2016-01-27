5ddd45a6defd292563ff647324e6e2401d23f837
Ivan <iv4art@ya.ru>
2016-01-27 15:53:19 +0000

    Добавлен шаблон табов

---
+++ src/resources/views/_utils/tabs.blade.php
@@ -0,0 +1,7 @@
+<ul class="nav nav-tabs">
+	@foreach ($tabs as $tab_title => $tab_route)
+		<li {!! $tab_route == Route::currentRouteName() ? ' class="active"' : NULL !!}>
+			<a href="{{ route($tab_route) }}">{{ $tab_title }}</a>
+		</li>
+	@endforeach
+</ul>
