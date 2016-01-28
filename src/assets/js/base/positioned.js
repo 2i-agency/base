36728ce67155dfd0d36b3555de87ac439efd172a
Ivan <iv4art@ya.ru>
2016-01-28 03:55:58 +0300

    Обновлён JS-скрипт для позиционирования

--- src/assets/js/base/positioned.js
+++ src/assets/js/base/positioned.js
@@ -1,5 +1,34 @@
 $(function(){
 
-	
+	$('.positioned').sortable({
+		update: function(e, ui){
+			// DOM elements
+			var $elem = $(ui.item[0]);
+			var $wrapper = $elem.closest('.positioned');
+
+			// Data
+			var id = $elem.data('id');
+			var ids = $wrapper.sortable('toArray', {attribute: 'data-id'});
+
+			// Finding position
+			for (var i = 0; i < ids.length; i++)
+			{
+				if (id == ids[i])
+				{
+					var position = i + 1;
+					break;
+				}
+			}
+
+			// Sending request
+			$.ajax({
+				url: $wrapper.data('url'),
+				data: {
+					id: id,
+					position: position
+				}
+			});
+		}
+	});
 
 });
