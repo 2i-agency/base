2fb72c8574e5e552b9f7fa9bb30bdcab65b73f5c
iv4art <iv4art@ya.ru>
2016-06-05 02:11:47 +0300

    Из хлебных крошек в разделе пользователей удалены иконки

--- resources/views/admin/users/_breadcrumbs.blade.php
+++ resources/views/admin/users/_breadcrumbs.blade.php
@@ -1,15 +1,4 @@
 <ul class="breadcrumb">
-	<li>
-		<span class="fa fa-list"></span>
-		<a href="{{ route('admin.users') }}">Пользователи</a>
-	</li>
-	<li class="active">
-		@if (isset($user))
-			<span class="fa fa-pencil"></span>
-			{{ $user->getName() }}
-		@else
-			<span class="fa fa-plus"></span>
-			Добавление пользователя
-		@endif
-	</li>
+	<li><a href="{{ route('admin.users') }}">Пользователи</a></li>
+	<li class="active">{{ isset($user) ? $user->getName() : 'Добавление пользователя' }}</li>
 </ul>
