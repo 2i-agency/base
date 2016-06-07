6f1556ea706aae10aee248736ac1f63478786b80
iv4art <iv4art@ya.ru>
2016-06-07 21:28:52 +0300

    Переименован трейт для сравнения моделей

--- app/Models/Traits/Comparable.php
+++ app/Models/Traits/Comparable.php
@@ -4,10 +4,11 @@ namespace Chunker\Base\Models\Traits;
 
 use Illuminate\Database\Eloquent\Model;
 
-// TODO Вереименовать трейт Comparison в Comparable
-trait Comparison
+trait Comparable
 {
-	// Сравнение с другой моделью
+	/*
+	 * Сравнение с другой моделью
+	 */
 	public function is(Model $model) {
 		$is_class_equivalent = get_class($this) == get_class($model);
 		$is_key_equivalent = $this->getKey() == $model->getKey();
