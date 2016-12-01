ec4565652bded49fd120ea588f950fedafcba05f
aboldyrev <boldyrev.a.s@studio2i.ru>
2016-12-01 10:22:38 +0300

    Поправил language.js для правильно работы с флагами

--- resources/assets/js/languages.js
+++ resources/assets/js/languages.js
@@ -11,9 +11,16 @@ $(function(){
 		var action_update = $button.data('action_update'),
 			name = $button.data('name'),
 			locale = $button.data('locale'),
+			flag = $button.data('flag'),
 			is_published = Number($button.data('is_published'));
 
 		// Настройка окна
+		if (flag.length) {
+			$('.js-flag-modal img').attr('src', flag);
+			$('.js-flag-modal').removeClass('hidden');
+		} else {
+			$('.js-flag-modal').addClass('hidden');
+		}
 		$modal.attr('action', action_update);
 		$modal.find('input[name="name"]').val(name);
 		$modal.find('input[name="locale"]').val(locale);
