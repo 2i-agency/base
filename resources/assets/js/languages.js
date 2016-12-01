120952e4b699a927339a4b92887211401bd4ea2a
aboldyrev <boldyrev.a.s@studio2i.ru>
2016-12-01 10:44:10 +0300

    Подправил оформление флага при редактировании языка

--- resources/assets/js/languages.js
+++ resources/assets/js/languages.js
@@ -12,19 +12,21 @@ $(function(){
 			name = $button.data('name'),
 			locale = $button.data('locale'),
 			flag = $button.data('flag'),
+			flag_image = $('.js-flag-modal'),
 			is_published = Number($button.data('is_published'));
 
 		// Настройка окна
-		if (flag.length) {
-			$('.js-flag-modal img').attr('src', flag);
-			$('.js-flag-modal').removeClass('hidden');
-		} else {
-			$('.js-flag-modal').addClass('hidden');
-		}
 		$modal.attr('action', action_update);
 		$modal.find('input[name="name"]').val(name);
 		$modal.find('input[name="locale"]').val(locale);
 		$modal.find('input[name="is_published"][value="' + is_published + '"]').attr('checked', '');
+		// Настройки отображения флага
+		if (flag.length) {
+			flag_image.attr('src', flag);
+			flag_image.removeClass('hidden');
+		} else {
+			flag_image.addClass('hidden');
+		}
 	})
 
 });
