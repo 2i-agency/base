1725fbd20ee399e15886e1e5a50c13cb671b6fb7
boldyrev.a <boldyrev.a@studio2i.ru>
2016-12-23 16:10:17 +0300

    Рефакторинг команд

--- app/Commands/Seed.php
+++ app/Commands/Seed.php
@@ -4,21 +4,26 @@ namespace Chunker\Base\Commands;
 
 use Illuminate\Console\Command;
 
+/**
+ * Class Seed. Команда для посева данных в БД
+ *
+ * @package Chunker\Base\Commands
+ */
 class Seed extends Command
 {
-	protected $description = 'Seeding data for Chunker';
 	protected $signature = 'chunker:seed
 		{--package : Seeding data only for one package}';
+	protected $description = 'Seeding data for Chunker';
 
 
-	public function handle() {
+	public function handle(){
 		$seeders = app()
 			->make('Packages')
 			->getSeeders();
-		
+
 		if (count($seeders)) {
 			foreach ($seeders as $seeder) {
-				$this->call('db:seed', ['--class' => $seeder]);
+				$this->call('db:seed', [ '--class' => $seeder ]);
 			}
 
 			$this->line('Data were sown');
