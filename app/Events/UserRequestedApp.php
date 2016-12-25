8d1123951be374d475192d2a689a3040dd4dd524
boldyrev.a <boldyrev.a@studio2i.ru>
2016-12-25 18:16:09 +0300

    Мелкие правки комментариев

--- app/Events/UserRequestedApp.php
+++ app/Events/UserRequestedApp.php
@@ -6,13 +6,13 @@ use App\Events\Event;
 use Chunker\Base\Models\User;
 
 /**
- * Class UserRequestedApp - Событие при обращении пользователя к приложению
+ * Class UserRequestedApp - Событие при совершении действий пользователем
  *
  * @package Chunker\Base\Events
  */
 class UserRequestedApp extends Event
 {
-	/** @var User $user Пользователь проходящий аутентификацию */
+	/** @var User $user Пользователь, от имени которого происходит совершается действие */
 	public $user;
 
 
