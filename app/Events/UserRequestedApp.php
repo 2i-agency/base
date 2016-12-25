361aba1edc916de021751f5bc3acffe642200c6c
boldyrev.a <boldyrev.a@studio2i.ru>
2016-12-25 17:30:35 +0300

    Рефакторинг событий

--- app/Events/UserRequestedApp.php
+++ app/Events/UserRequestedApp.php
@@ -5,12 +5,18 @@ namespace Chunker\Base\Events;
 use App\Events\Event;
 use Chunker\Base\Models\User;
 
+/**
+ * Class UserRequestedApp - Событие при обращении пользователя к приложению
+ *
+ * @package Chunker\Base\Events
+ */
 class UserRequestedApp extends Event
 {
+	/** @var User $user Пользователь проходящий аутентификацию */
 	public $user;
 
 
-	public function __construct(User $user) {
+	public function __construct(User $user){
 		$this->user = $user;
 	}
 }
