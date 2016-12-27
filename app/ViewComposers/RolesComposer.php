a3a95148b7868520ed5a477a8903c68ab62d9f1d
boldyrev.a <boldyrev.a@studio2i.ru>
2016-12-27 17:55:42 +0300

    Рефакторинг композеров

--- app/ViewComposers/RolesComposer.php
+++ app/ViewComposers/RolesComposer.php
@@ -5,12 +5,18 @@ namespace Chunker\Base\ViewComposers;
 use Chunker\Base\Models\Role;
 use Illuminate\View\View;
 
+/**
+ * В представление передаётся переменная $_roles
+ * хранящая в себе список всех ролей
+ *
+ * @package Chunker\Base\ViewComposers
+ */
 class RolesComposer
 {
-	public function compose(View $view) {
+	public function compose(View $view){
 		$_roles = Role
 			::orderBy('name')
-			->get(['id', 'name']);
+			->get([ 'id', 'name' ]);
 
 		$view->with('_roles', $_roles);
 	}
