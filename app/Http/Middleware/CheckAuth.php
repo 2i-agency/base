a3a3731d3752600d34a2f5f0086e6548dbfdf39d
boldyrev.a <boldyrev.a@studio2i.ru>
2016-12-29 10:34:35 +0300

    Рефакторинг посредников

--- app/Http/Middleware/CheckAuth.php
+++ app/Http/Middleware/CheckAuth.php
@@ -5,31 +5,47 @@ namespace Chunker\Base\Http\Middleware;
 use Chunker\Base\Events\UserRequestedApp;
 use Closure;
 use Auth;
+use Illuminate\Http\Request;
 
+/**
+ * Посредник проверки авторизации и прав пользователя
+ *
+ * @package Chunker\Base\Http\Middleware
+ */
 class CheckAuth
 {
-	public function handle($request, Closure $next) {
-		// Если пользователь не авторизован
+	/**
+	 * Функция отправки ответа
+	 *
+	 * @param Request    $request запрос на который необходимо отправить ответ
+	 * @param string     $message сообщение для ответа
+	 * @param int|string $status код статуса
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	protected function responses($request, $message, $status){
+		if ($request->ajax()) {
+			/** Ответ при асинхронном запросе */
+			return response($message, $status);
+		} else {
+			/** Ответ при GET-запросе */
+			return response()->view('chunker.base::admin.auth.login', [], $status);
+		}
+	}
+
+
+	public function handle($request, Closure $next){
+		/** Если пользователь не авторизован */
 		if (Auth::guest()) {
 			return $this->responses($request, 'Unauthorized', 401);
-		} elseif (!Auth::user()->isAdmin()) {
+		} /** Если у пользователя нет прав */
+		elseif (!Auth::user()->isAdmin()) {
 			return $this->responses($request, 'Forbidden', 403);
-		} else {
-			// Регистрация запроса пользователя
+		} /** Регистрация запроса пользователя */
+		else {
 			event(new UserRequestedApp(Auth::user()));
 		}
 
 		return $next($request);
-	}
-
-
-	protected function responses($request, $message, $status) {
-		if ($request->ajax()) {
-			// Ответ при асинхронном запросе
-			return response($message, $status);
-		} else {
-			// Ответ при GET-запросе
-			return response()->view('chunker.base::admin.auth.login', [], $status);
-		}
 	}
 }
