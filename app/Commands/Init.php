fe40f3a7dc1f99e35212c738bee7306f95079ea3
boldyrev.a <boldyrev.a@studio2i.ru>
2017-01-20 16:05:11 +0300

    Убрал из команды chunker:init переименование миграции из пакета medialibrary

--- app/Commands/Init.php
+++ app/Commands/Init.php
@@ -63,19 +63,6 @@ class Init extends Command
 			// Публикация ассетов пакетов
 			$this->call('vendor:publish', [ '--force' => true ]);
 
-			// Переименование миграции медиафайлов
-			$media_migration_search_results = glob(database_path('migrations/*_create_media_table.php'));
-
-			if ($media_migration_search_results) {
-				$media_migration_filename = $media_migration_search_results[ 0 ];
-				$new_media_migration_filename = 'migrations/2016_00_00_000000_create_media_table.php';
-				rename(
-					$media_migration_filename,
-					database_path($new_media_migration_filename));
-
-				$this->info('Rename media migration');
-			}
-
 			// Обновление кеша автозагрузки классов
 			`composer dump-autoload`;
 
