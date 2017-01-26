edbcb1ead88b183f6c0e830d5561d0c630b27f38
boldyrev.a <boldyrev.a@studio2i.ru>
2017-01-26 12:43:21 +0300

    Поправил представление тригера возможностей

--- resources/views/utils/ability-trigger.blade.php
+++ resources/views/utils/ability-trigger.blade.php
@@ -1,8 +1,4 @@
-@php($namespace = Chunker\Base\Models\Ability::detectNamespace($ability))
-@php($disabled = \Auth::user()->hasAdminAccess($namespace) ? NULL : ' disabled')
-
 <option
 	value="{{ str_contains($ability, '.') ? $ability : 0 }}"
     {{ $is_selected ? ' selected' : NULL }}
-	{{ $disabled }}
 >{{ $label }}</option>
