e9fbe08e64a00b98866f989b4862747c60d07bbf
boldyrev.a <boldyrev.a@studio2i.ru>
2017-02-01 19:41:13 +0300

    В трейт RelationAgents добавил заготовку запроса для фильтрации по доступным

--- app/Models/Traits/RelationAgents.php
+++ app/Models/Traits/RelationAgents.php
@@ -8,4 +8,22 @@ trait RelationAgents
 	public function agents() {
 		return $this->morphMany(Agent::class, 'model');
 	}
+
+	public function ScopeOnlyAccess($query, $ability) {
+
+		if (\Auth::user()->id == 1) {
+			return $query;
+		}
+
+		$allow_access = \Auth
+			::user()
+			->agents()
+			->where('model_type', get_class($this))
+			->where('ability_id', $ability)
+			->pluck('model_id')
+			->toArray();
+
+		return $query->whereIn('id', $allow_access);
+
+	}
 }
