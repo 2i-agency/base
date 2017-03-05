0bc5bebef2c9acd8e204160b6cfcb1f45ac4b2d1
iv4art <iv4art@ya.ru>
2017-03-06 00:27:55 +0300

    Добавлена инициализация подтверждения удаления

--- resources/assets/js/base/init.js
+++ resources/assets/js/base/init.js
@@ -158,4 +158,12 @@ $(function(){
 		tinymce.init(get_tinymce_config());
 	}
 
+
+	/*
+	 * Кнопки удаления
+	 */
+	$('.js-btn-delete').closest('form').submit(function() {
+		return confirm('Вы уверены, что хотите удалить элемент?');
+	});
+
 });
