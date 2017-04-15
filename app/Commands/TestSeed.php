08fa3eff1b978de8f3f1758293eefee6459f5874
boldyrev.a <boldyrev.a@studio2i.ru>
2017-04-15 17:44:48 +0300

    Добавил команду для тестовых посевов

---
+++ app/Commands/TestSeed.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Chunker\Base\Commands;
+
+use Illuminate\Console\Command;
+
+/**
+ * Команда для посева данных в БД
+ *
+ * @package Chunker\Base\Commands
+ */
+class TestSeed extends Command
+{
+	/** @var string конамда для консоли */
+	protected $signature = 'chunker:test-seed';
+	/** @var string описание команды */
+	protected $description = 'Посев тестовых данных для пакетов Chunker';
+
+
+	public function handle() {
+		// Проверяем окружение
+		if (!env('APP_DEBUG')) {
+			$this->error('Проект в продакшне! Тестовый посев невозможен.');
+			return false;
+		}
+
+		// Предупреждаем об очистке БД
+		if (!$this->confirm('База данных будет очищена. Вы действительно хотите продожить?')) {
+			return false;
+		}
+
+		// Чистим таблицы
+		\DB::table('media')->truncate();
+		\DB::table('activity_log')->truncate();
+		\DB::table('base_authentications')->truncate();
+
+		// Собираем классы посевщиков
+		$seeders = app()
+			->make('Packages')
+			->getTestSeeders();
+
+		// Сеям тестовые данные
+		if (count($seeders)) {
+			foreach ($seeders as $seeder) {
+				$this->callSilent('db:seed', [ '--class' => $seeder ]);
+				$this->line('<info>Посеяны: </info>' . $seeder);
+			}
+
+			$this->info('Все данные посеяны.');
+		}
+
+		return true;
+	}
+}
