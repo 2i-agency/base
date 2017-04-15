e6070480e9b193bb9f2d22964af90b87fb75b841
boldyrev.a <boldyrev.a@studio2i.ru>
2017-04-15 19:16:47 +0300

    поправил фабрику модели Redirect

--- publishes/database/factories/RedirectFactory.php
+++ publishes/database/factories/RedirectFactory.php
@@ -2,7 +2,7 @@
 
 $factory->define(\Chunker\Base\Models\Redirect::class, function (Faker\Generator $faker) {
 	return [
-		'from' => '/' . $faker->unique()->word,
+		'from' => '/' . $faker->unique()->slug(1),
 		'to' => $faker->url,
 		'comment' => $faker->sentence(3),
 		'is_active' => $faker->boolean
